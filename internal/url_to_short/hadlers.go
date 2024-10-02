fea4f8b2ba33f05e7778497429e7dcde3a537563
Bitummit <ignat.700@mail.ru>
2024-10-02 23:13:39 +0300

    add: new alis hander

--- internal/url_to_short/hadlers.go
+++ internal/url_to_short/hadlers.go
@@ -2,23 +2,45 @@ package urltoshort
 
 import (
 	"go_api/internal/storage"
+	"go_api/pkg/handler_utils"
+	"go_api/pkg/logger"
 	"log/slog"
 	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 )
 
-type Request struct {
+type NewAliasRequest struct {
 	URL string `json:"url" validate:"required,url"`
 }
 
-type Response struct {
-	Status string `json:"status"`
-	Error string `json:"error,omitempty"`
+type NewAliasResponse struct {
+	handler_utils.Response
 	Alias string `json:"alias,omitempty"`
 }
 
 
-func NewAlias(log *slog.Logger, urlSaver storage.QueryFunctions) http.HandlerFunc {
+func NewAlias(log *slog.Logger, urlSaveQuery storage.QueryFunctions) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		const place = "handlers.newAlias"
+		log = log.With(
+			slog.String("handler", place),
+			slog.String("reques_id", middleware.GetReqID(r.Context())),
+		)
+	
+		var req NewAliasRequest
+	
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", logger.Err(err))
+			render.JSON(w, r, handler_utils.Error("failed to decode request"))
+	
+			return
+		}
+	
+		log.Info("Request decoded", slog.Any("request", req))
+		
 	}
 }
+
